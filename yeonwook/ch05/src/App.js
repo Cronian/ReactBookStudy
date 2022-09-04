6387cbfc275c90887cce5295d5470e3e0ba0c538
yeonwook-jung <yeonwook.jung@navercorp.com>
2022-09-04 19:03:37 +0900

    ch05, ch06 스터디

---
+++ yeonwook/ch05/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
+
+class App extends Component {
+  render() {
+    return (
+        // <ValidationSample/>
+        <div>
+          <ScrollBox/>
+        </div>
+    );
+  }
+}
+
+export default App;
