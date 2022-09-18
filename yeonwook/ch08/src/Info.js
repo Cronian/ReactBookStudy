d82fa03b0797d0379807c5f2476c1a010bf7996e
yeonwook-jung <yeonwook.jung@navercorp.com>
2022-09-18 17:41:39 +0900

    ch08

---
+++ yeonwook/ch08/src/Info.js
@@ -0,0 +1,70 @@
+import {useEffect, useState} from "react";
+// import {useReducer} from "react";
+import useInputs from './useInputs';
+
+// function reducer(state, action) {
+//     return  {
+//         ...state,
+//         [action.name]: action.value
+//     };
+// }
+
+const Info = () => {
+    // const [name, setName] = useState('');
+    // const [nickname, setNickname] = useState('');
+
+    // useEffect(() => {
+    //     console.log('effect');
+    //     // console.log({
+    //     //     name,
+    //     //     nickname
+    //     // });
+    //     return () => {
+    //         console.log('unmount');
+    //         // console.log(name);
+    //     }
+    // }, []);
+    //
+    // const onChangeName = e => {
+    //     setName(e.target.value);
+    // };
+
+    // const onChangeNickname = e => {
+    //     setNickname(e.target.value);
+    // };
+
+    // const [state, dispatch] = useReducer(reducer, {
+    //     name: '',
+    //     nickname: ''
+    // });
+    //
+    // const { name, nickname }= state;
+    // const onChange = e => {
+    //     dispatch(e.target);
+    // }
+
+    const [state, onChange] = useInputs({
+        name: '',
+        nickName: ''
+    });
+    const { name, nickname } = state;
+
+    return (
+        <div>
+            <div>
+                <input name={"name"} value={name} onChange={onChange} />
+                <input name={"nickname"} value={nickname} onChange={onChange}/>
+            </div>
+            <div>
+                <div>
+                    <b>이름:</b> {name}
+                </div>
+                <div>
+                    <b>닉네임: </b> {nickname}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Info;
