98783843df3bd6460a9cbd05fc0cf3833357fff0
hyunjungkimm <khjjung1124@gmail.com>
2022-09-21 07:31:03 +0900

    [hyunjung] ch08 upload

---
+++ hyunjung/ch08/src/Info.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+
+const Info = () => {
+    const[name, setName] = useState('');
+    const[nickname, setNickname] = useState('');
+    useEffect(() => {
+        console.log('effect');
+        return() => {
+            console.log('unmount');
+        };
+    }, []);
+    /*
+    useEffect(() => {
+        console.log('effect');
+        console.log(name);
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        };
+    }, [name]);
+    */
+    /*
+    console.log('렌더링이 완료되었습니다!');
+        console.log({
+            name,
+            nickname
+        })
+     */
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    };
+
+    const onChangeNickname = e => {
+        setNickname(e.target.value);
+    }
+
+    return(
+        <div>
+            <div>
+                <input value={name} onChange={onChangeName}/>
+                <input value={nickname} onChange={onChangeNickname}/>
+            </div>
+            <div>
+                <div>
+                    <b>이름:</b> {name}
+                </div>
+                <div>
+                    <b>닉네임:</b> {nickname}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
