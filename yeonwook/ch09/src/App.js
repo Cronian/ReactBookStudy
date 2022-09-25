8b0b03e81387c6c7f91a939b60044e49ee8eef7f
yeonwook-jung <yeonwook.jung@navercorp.com>
2022-09-25 20:30:56 +0900

    ch09, ch10

---
+++ yeonwook/ch09/src/App.js
@@ -0,0 +1,29 @@
+// import { Component } from "react";
+// import SassComponent from "./SassComponent";
+//
+// class App extends Component {
+//   render() {
+//     return (
+//         <div>
+//           <SassComponent/>
+//         </div>
+//     )
+//   }
+// }
+//
+// export default App;
+
+import { Component } from "react";
+import CSSModule from './CSSModule'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <CSSModule/>
+            </div>
+        );
+    }
+}
+
+export default App;
