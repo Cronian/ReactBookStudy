602343c869f69d64a553441cd1183c2e07ff7105
Cronian <cronian89@gmail.com>
2022-10-10 18:04:56 +0900

    feat ch13 example
    ch13 13.4.2 queryString 커밋

--- jinwoong/ch13/router-tutorial/src/App.js
+++ jinwoong/ch13/router-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
       </Routes>
   );
 }
